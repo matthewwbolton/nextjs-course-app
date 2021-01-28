52dde88ce228e7b929754a959cee7af9ca6e9143
Matthew Bolton <matthewwbolton@gmail.com>
2021-01-28 11:47:12 -0500

    completed setup and config of client side routing with next/link

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,16 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
-export default () => <h1>Index Page</h1>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Index Page</h1>
+
+      <Link href="/notes">
+        <a>Notes</a>
+      </Link>
+    </div>
+  );
+};
+
+export default IndexPage;
--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,3 +1,26 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
-export default () => <h1>Notes</h1>
+const NotesIndex = () => {
+  const notes = new Array(15)
+    .fill(1)
+    .map((e, i) => ({ id: i, title: `Note: ${i}` }));
+
+  return (
+    <div>
+      <h1>Notes</h1>
+
+      {notes.map((note) => (
+        <div>
+          <Link key={note.id} href="notes/[id]" as={`/notes/${note.id}`}>
+            <a>
+              <strong>{note.title}</strong>
+            </a>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default NotesIndex;
